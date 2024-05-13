a2daf91be9f2fea0c6d2f1f8fa299bc4609fb6b5
EAL25 <Evelyn.AhLing25@ycdsbk12.ca>
2024-05-13 14:12:42 -0400

    Finale

--- Sketch.java
+++ Sketch.java
@@ -2,6 +2,8 @@ import processing.core.PApplet;
 
 public class Sketch extends PApplet {
 	
+  float circleX = 150;
+    float circleY = 150;
 	
   /**
    * Called once at the beginning of execution, put your size all in this method
@@ -9,6 +11,7 @@ public class Sketch extends PApplet {
   public void settings() {
 	// put your size call here
     size(400, 400);
+    
   }
 
   /** 
@@ -16,21 +19,50 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+    background(0);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+	  if (mousePressed) {
+      fill(0, 255, 255);
+      ellipse(mouseX, mouseY, 25, 25);
+    }
+    
+    ellipse(mouseX, mouseY, 50, 50);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
-  }
+    if (keyPressed){
+      if (keyCode == UP){
+        circleY--;
+      }
+      else if (keyCode == DOWN){
+        circleY++;
+      }
+      else if(keyCode == LEFT){
+        circleX--;
+      }
+      else if(keyCode == RIGHT){
+        circleX++;
+      }
+      ellipse(circleX, circleY, 50, 50);
+      fill(168, 74, 50);
+    }
+
+   
+
   
   // define other methods down here.
+}
+  public void mouseDragged(){
+      fill(252, 186, 3);
+      rect(mouseX, mouseY, 25, 25);
+    }
+
+    public void mouseWheel(){
+      fill(255, 255, 255);
+      rect(mouseX, mouseY, 25, 25);
+    }
+
 }
